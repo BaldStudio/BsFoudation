7bfb9451e2ffe6522242af80ed76232f1cc492b8
crzorz <crzorz@outlook.com>
2023-03-30 10:33:25 +0800

    随便提交点什么

--- BsFoundation/Source/SwiftPlus/UIColor.swift
+++ BsFoundation/Source/SwiftPlus/UIColor.swift
@@ -66,3 +66,19 @@ public extension SwiftPlus where T: UIColor {
                 alpha: 1.0)
     }
 }
+
+#if DEBUG
+
+public extension SwiftPlus where T: UIColor {
+    var literial: String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        this.getRed(&r, green: &g, blue: &b, alpha: &a)
+        return "#colorLiteral(red: \(r), green: \(g), blue: \(b), alpha: \(a))"
+    }
+
+}
+
+#endif
